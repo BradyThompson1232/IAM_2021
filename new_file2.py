8e3fcf1eae4c6c1a5ffacc0483b6cb1100bf6f9c
Brady Thompson <thombrad@chritmac311.chrobinson.com>
2021-07-13 09:52:15 -0500

    additional changes

--- new_file2.py
+++ new_file2.py
@@ -7,3 +7,17 @@
 # dsfdfs
 # asfa
 #  dsfasf
+
+# these are the new round of changes brady wants to make
+# sifaew
+# asdfawe
+# fasf
+# try:
+#     ge
+#     age
+# except expression as identifier:
+#     pass
+# else:
+#     pass
+# finally:
+#     pass
